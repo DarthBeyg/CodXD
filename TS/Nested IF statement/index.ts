cd730d5d1256878b87b4a5b7b04d9935adef9d8e
AbdulRub <abdulrubbaig@gmail.com>
2024-04-03 00:00:37 +0500

    check

--- TS/Nested IF statement/index.ts
+++ TS/Nested IF statement/index.ts
@@ -1,14 +1,22 @@
-let userName = "Bilal";
-let passWord = 123;
-let securityCode = 456;
+let userName = "Ahmed";
+let passWord = 111;
+let securityCode = 789;
  
-if (userName == "bilal" && passWord == 123) {
-  console.log("Please wait, authenticating security code");
+if (userName == "Bilal") {
+  console.log("Please wait, authenticating security code..");
   if(securityCode == 456){
     console.log(`Hi ${userName}, Your Password is ${passWord}`); 
   }
   else {
-    console.log("Security code authenticating failed.");
+    console.log("Security code authentication failed.");
+  }
+} else if  (userName == "Ahmed") {
+  console.log("Please wait, authenticating security code..");
+  if(securityCode == 789){
+    console.log(`Hi ${userName}, Your Password is ${passWord}`); 
+  }
+  else {
+    console.log("Security code authentication failed.");
   }
 } else {
   console.log("Invalid Username or Password");
