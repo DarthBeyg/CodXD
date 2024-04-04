8576a07931a5f48275870505e9d0937db8dc0595
AbdulRub <abdulrubbaig@gmail.com>
2024-04-04 15:20:18 +0500

    blabla

--- Random/index.ts
+++
@@ -1,6 +0,0 @@
-let year: number = 2023; // Replace with the year you want to check
-
-// A year is a leap year if it is divisible by 4 but not by 100, unless it's also divisible by 400
-let isLeapYear: boolean = (year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0);
-
-console.log(`${year} is ${isLeapYear ? 'a leap year' : 'not a leap year'}.`);
