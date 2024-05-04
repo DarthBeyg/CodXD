da2269651b362fe0697b8d7d54d89024157d9fae
AbdulRub <abdulrubbaig@gmail.com>
2024-05-04 21:06:13 +0500

    updated

--- TS/Class/Class1/index.js
+++ TS/Class/Class1/index.js
@@ -12,7 +12,7 @@ greet();
 function greetWithParameters(name) {
     console.log(`Hello there ${name}`);
 }
-greetWithParameters(`Ali`);
+greetWithParameters("Ali");
 //Function with Return statement
 function abc() {
     console.log(`This is abc function`);
@@ -35,14 +35,15 @@ console.log(fruits[1]);
 //1. push(adds) add at the end
 const aa = fruits.push('Mangoes');
 console.log(fruits);
-console.log(aa);
+console.log(aa); // fruits.push returns length of an array
 //2. Pop(deletes) deletes at the end
 const bb = fruits.pop();
 console.log(fruits);
-console.log(bb);
+console.log(bb); // fruits.pop returns the value deleted at the end of an array
 //3. Unshift(adds) adds at the start
-fruits.unshift('Cherries');
+const cc = fruits.unshift('Cherries');
 console.log(fruits);
+console.log(cc); // stores new length of array
 //4. shift(deletes) removes from the start
 fruits.shift();
 console.log(fruits);
@@ -50,8 +51,10 @@ const fruits1 = ['Apples', 'Oranges', 'Bananas', 'Apples1', 'Bananas1', 'Oranges
 //3. Splice adds and/or remove from a specific index  but also manipulates original array 
 fruits1.splice(1, 0, 'cherries');
 console.log(fruits1);
-fruits1.splice(1, 6, 'Watermelons');
+const dd = fruits1.splice(1, 6, 'Watermelons');
 console.log(fruits1);
-//4. Slice "Selects" elements from specific index in an array but does not manipulate original array.
+console.log(dd); //splice method in itself returns an array with deleted value
+//4. Slice "Selects" elements from specific index & makes it into a new array but does not manipulate original array.
 const a1 = fruits1.slice(1);
+console.log(fruits1);
 console.log(a1);
