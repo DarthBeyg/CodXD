8dc34499b05cc8c345fd5f34e110ce4addca94c4
AbdulRub <abdulrubbaig@gmail.com>
2024-05-17 17:40:33 +0500

    ok

--- TS/Assignment/Assignment3/index.ts
+++ TS/Assignment/Assignment3/index.ts
@@ -2,7 +2,7 @@ let favMovie: string = "Matrix Reloaded";
 let firstName = "AbdulRub";
 let lastName = "Baig";
 let age: number = 31;
-console.log(`Hi, this is ${firstName} and i am ${age} years old.`);
+console.log(`Hi, this is ${firstName} and i am ${age} years old.`); //template string
 const currentMood_good: boolean = true;
 console.log(`Full Name: ${firstName} ${lastName}`);
 console.log(`Product name: 1KG Sugar 
